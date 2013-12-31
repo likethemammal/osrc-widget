3ee46372cf9cf9abfb2b06d57812bc083f624e64
Chris Dolphin <chris.dolphin@escapemg.com>
2013-12-31 00:41:26 -0500

    added an example.

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+require('server/fetch-json.php');
+
+?>
+
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+
+<div id="github-container">
+    <svg width="200" height="210">
+
+    </svg>
+</div>
+
+</body>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="osrc-widget.js"></script>
+</html>

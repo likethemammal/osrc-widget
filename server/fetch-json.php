b6ad0086cd7ed7771855e2b1c58e75f6efb2db83
Chris Dolphin <dolphin@likethemammal.com>
2014-01-24 03:19:20 -0500

    Remove old example.

--- server/fetch-json.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$username = "Likethemammal"; // <-- Your username here
-$url = "http://osrc.dfm.io/$username.json";
-$JSON = file_get_contents($url);
-
-echo "<script>  window.osrc = { stats: $JSON };  </script>";
-
-?>
